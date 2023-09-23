d524ec1d1d4c67a87194262095c4e4ac195a009c
Jontzey <Jonathan.andersson1997@outlook.com>
2023-09-23 12:08:24 +0200

    added css to specific gameId
    you can now access specific game when searching in the searchbar and pressing the game u want to know about.
    
    It sends the id to method and applies that to a route then we take the parameter which is a id and use that to find which object in database it is and save that in a variable and can later use in both front/backend.

--- src/app/Routes/game-id/game-id.component.ts
+++ src/app/Routes/game-id/game-id.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { GameServiceService } from 'src/app/services/game-service.service';
 @Component({
   selector: 'app-game-id',
   templateUrl: './game-id.component.html',
   styleUrls: ['./game-id.component.css']
 })
-export class GameIdComponent {
+export class GameIdComponent implements OnInit {
 
+  constructor(private route:ActivatedRoute, private service:GameServiceService){
+
+  }
+
+  singleGamedId:number= 0;
+  singleGame:any = {};
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.singleGamedId = +params['id'];
+
+      this.service.getGameById(this.singleGamedId).subscribe((data) => {
+        this.singleGame = data;
+      })
+    })
+  }
 }
